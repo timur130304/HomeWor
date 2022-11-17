374f2eecaaae893636f19d0c656e5081837a5090
Timur Aslanov <timurckaaslanov@yandex.ru>
2022-11-17 11:43:03 +0300

    HomeWork2

--- Program.cs
+++ Program.cs
@@ -1,66 +1,91 @@
-﻿//Задача 25
+﻿// Задачча 34
 
-// int Degree(int one, int two){
-//     int degree = one;
-//     if (two <= 0){
-//         degree = 0;
-//     }
-//     else{
-//         for (int i = 1;i < two; i++){
-//             degree *= one;
-//         }
-//     }
-//     return degree;
-// }
-
-// Console.WriteLine("Enter a number: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Enter the degree of the number: ");
-// int b = Convert.ToInt32(Console.ReadLine());
-// int deg  = Degree(a, b);
-// if (deg == 0){
-//     Console.WriteLine($"The degree b must be natural");
-// }
-// else{
-//     Console.WriteLine($"The number {a} to the power of {b}  = {deg}");
-// }
-
-// Задача 27
-
-// int TheAmount(int a)
-// {
-//     int i = a, sum = 0;
-//     int count = 0;
-//     while (i > 0){
-//         i /= 10;
-//         count++;
-//     }
-//     for (int z  = 0;z < count;z++){
-//         sum  += a % 10;
-//         a /= 10;
-//     }
-//     return sum;
-// }
-
-// Console.WriteLine("Enter a number: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int amount = TheAmount(num);
-// Console.WriteLine($"The sum of the digits of the power of {num} is equal to {amount}"); 
-
-// Задача 29
-
-// void TheArray()
+// int [] CreateRandArray(int size)
 // {
 //     Random rnd = new Random();
-//     Console.WriteLine("Enter the size of the array: ");
-//     int size = Convert.ToInt32(Console.ReadLine());
 //     int [] arr  = new int [size];
 //     for (int i = 0;i < size;i++){
-//         arr[i] = rnd.Next(0,99);
-//         Console.Write(' ');
-//         Console.Write(arr[i]);
+//         arr[i] = rnd.Next(100,999);
 //     }
-
+//     return arr;
 // }
-// TheArray();
+// Console.WriteLine("Enter the size of the array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
 
+// static void PrintArray(int [] arr){
+//     for (int i = 0;i < arr.Length;i++){
+//         Console.WriteLine(arr[i]);
+//     }
+// }
+
+// void HonestNumber(int [] arr){
+//     int count = 0;
+//     for (int i = 0;i < arr.Length;i++){
+//         if (arr[i] % 2 == 0){
+//             count += 1;
+//         }
+//     }
+//     Console.WriteLine($"The number of even numbers in the array is {count}");
+    
+// }
+// PrintArray(CreateRandArray(size));
+// HonestNumber(CreateRandArray(size));
+
+// Задача 36
+
+// int [] CreateRandArray(int size)
+// {
+//     Random rnd = new Random();
+//     int [] arr  = new int [size];
+//     for (int i = 0;i < size;i++){
+//         arr[i] = rnd.Next(1,999);
+//         Console.WriteLine(arr[i]);
+//     }
+//     return arr;
+// }
+
+
+// void Sum(int [] arr){
+//     int sum = 0;
+//     for(int i = 0; i < arr.Length; i++){
+//         if (i % 2 != 0){
+//             sum += arr[i];
+//         }
+//     }
+//     Console.WriteLine($"The sum of the numbers in odd positions is equal to {sum}");
+// }
+// Console.WriteLine("Enter the size of the array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Sum(CreateRandArray(size));
+
+// Задача 38
+
+// double [] CreateRandArray(int size)
+// {
+//     Random rnd = new Random();
+//     double [] arr  = new double [size];
+//     for (int i = 0;i < size;i++){
+//         arr[i] = rnd.NextDouble();
+//         Console.WriteLine(arr[i]);
+//     }
+//     return arr;
+// }
+
+// void MaxElandMinEl(double [] arr ){
+//     double max = arr[0];
+//     double min = arr[0];
+//     for(int i = 1;i < arr.Length;i++){
+//         if (arr[i] > max){
+//             max = arr[i];
+//         }
+//     }
+//     for(int i = 1;i < arr.Length;i++){
+//         if (arr[i] < min){
+//             min = arr[i];
+//         }
+//     }
+//     Console.WriteLine($"The difference between  the max and min is equal to {max - min}");
+// }
+// Console.WriteLine("Enter the size of the array: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// MaxElandMinEl(CreateRandArray(size));
