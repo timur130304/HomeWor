7df7a555274fd503b54082359abd46977cf862c2
Timur Aslanov <timurckaaslanov@yandex.ru>
2022-11-24 17:48:05 +0300

    HomeWork4

--- Program.cs
+++ Program.cs
@@ -1,42 +1,98 @@
-﻿// Задача 41
+﻿// Задача 47
 
-// Console.WriteLine("Enter the size of the array: ");
-// int size = Convert.ToInt32(Console.ReadLine());
-// int [] arr  = new int [size];
-// void CreateArray()
-// {
-//     for (int i = 0;i < arr.Length;i++){
-//         Console.WriteLine($"Enter {i} array value:");
-//         arr[i] = Convert.ToInt32(Console.ReadLine());
-//     }
-// }
-// int i = 0, count = 0;
-// void NumberOfNumbers(int []arr){
-//     if (arr[i] > 0){
-//         count++;
-//         if(i < size - 1){
-//             i++;
-//             NumberOfNumbers(arr);
+// double [,] CreateandShowArray(){
+//     Console.WriteLine("Enter the numbers of rows: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Enter the numbers of columns: ");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Enter the minimum numbers: ");
+//     double min = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Enter the maximum numbers: ");
+//     double max = Convert.ToInt32(Console.ReadLine());
+//     double [,] arr = new double[rows, columns];
+//     for (int i = 0;i < rows;i++)
+//         for (int j = 0;j < columns; j++){
+//             arr[i,j] = new Random().NextDouble() * (max - min) + min;
 //         }
-//     }
-    
+//     return arr;
 // }
-// CreateArray();
-// NumberOfNumbers(arr);
-// Console.WriteLine($"The number of numbers greater than zero is {count}");
 
-// Задача 43
+// void ShowArray(double[,] arr){
+//     for (int i = 0;i < arr.GetLength(0);i++){
+//         for (int j = 0;j < arr.GetLength(1); j++){
+//             Console.Write(arr[i,j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+// ShowArray(CreateandShowArray());
 
-// Console.WriteLine("введите значение b1");
-// double b1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите число k1");
-// double k1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите значение b2");
-// double b2 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("введите число k2");
-// double k2 = Convert.ToInt32(Console.ReadLine());
+// Задача 50
 
-// double x = (-b2 + b1)/(-k1 + k2);
-// double y = k2 * x + b2;
+// int [,] CreateandShowArray(){
+//     Console.WriteLine("Enter the numbers of rows: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Enter the numbers of columns: ");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Enter the minimum numbers: ");
+//     int min = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Enter the maximum numbers: ");
+//     int max = Convert.ToInt32(Console.ReadLine());
+//     int [,] arr = new int[rows, columns];
+//     for (int i = 0;i < rows;i++)
+//         for (int j = 0;j < columns; j++){
+//             arr[i,j] = new Random().Next(min, max + 1);
+//         }
+//     return arr;
+// }
 
-// Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+// void ShowElementArray(int [,] arr){
+//     Console.WriteLine("Write the row number of the element: ");
+//     int row = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Write the column number of the element: ");
+//     int column = Convert.ToInt32(Console.ReadLine());
+//     if (row > 0 && row < arr.GetLength(0) && column > 0 && column < arr.GetLength(1)){
+//         Console.WriteLine($"Element: {arr[row,column]}");
+//     }
+//     else{
+//         Console.WriteLine("There is no such element");
+//     }
+// }
+// ShowElementArray(CreateandShowArray());
+
+// Задача 52
+
+int [,] CreateandShowArray(){
+    Console.WriteLine("Enter the numbers of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the numbers of columns: ");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the minimum numbers: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the maximum numbers: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int [,] arr = new int[rows, columns];
+    for (int i = 0;i < rows;i++)
+        for (int j = 0;j < columns; j++){
+            arr[i,j] = new Random().Next(min, max + 1);
+        }
+    return arr;
+}
+void ArithmeanandShowArr(int [,] arr){
+    for (int i = 0;i < arr.GetLength(0);i++){
+        for (int j = 0;j < arr.GetLength(1); j++){
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    for (int j = 0;j < arr.GetLength(1); j++){
+        double mean = 0;
+        for (int i = 0;i < arr.GetLength(0);i++){
+            mean += arr[i, j];
+        }
+        Console.WriteLine($"Среднее значение {j + 1} столбца: {mean / arr.GetLength(0)}");
+    }
+}
+ArithmeanandShowArr(CreateandShowArray());
